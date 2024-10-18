b39d82513d96f29444aab0726b1966eb8017a78f
Owhen-Min <skyblue848@naver.com>
2024-10-18 16:20:35 +0900

    models for board and comment

--- bulletin_board/boards/models.py
+++ bulletin_board/boards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    # author
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    # author
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
