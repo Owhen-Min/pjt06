71848491ab0654ad7357563cbb36420a9f19d874
Owhen-Min <skyblue848@naver.com>
2024-10-18 17:07:31 +0900

    create_comment function update

--- bulletin_board/boards/views.py
+++ bulletin_board/boards/views.py
@@ -26,7 +26,7 @@ def create(request):
 
 @require_http_methods(['POST'])
 def create_comment(request, board_pk):
-    board = get_object_or_404(board, board_pk)
+    board = get_object_or_404(board, pk=board_pk)
     comment = CommentForm(request.POST)
     if comment.is_valid():
         comment.save(commit=False)
